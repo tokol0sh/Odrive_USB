ef55ddafafcf8bc9d25f63db4040cf163234fd35
Byron <12byron34@gmail.com>
2018-03-08 00:30:49 +1300

    added a header only JSON library.

--- src/main.cpp
+++ src/main.cpp
@@ -3,102 +3,48 @@
 #include <string>
 #include <vector>
 #include "protocol.h"
+#include <iterator>
+#include <json.hpp>
 
+using nlohmann::json;
 #define VID     0x1209
 #define PID     0x0d31
+
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*x))
 
 
-void send_to_odrive(libusb_device_handle* handle, std::vector<uint8_t>& packet, int* sent_bytes) {
-	libusb_bulk_transfer(handle,
-		(1 | LIBUSB_ENDPOINT_OUT),
-		packet.data(),
-		packet.size(),
-		sent_bytes,
-		0);
-}
-
-void receive_from_odrive(libusb_device_handle* handle, std::vector<uint8_t>& packet, int max_bytes_to_receive, int* received_bytes, int timeout = 0) {
-	libusb_bulk_transfer(handle,
-		(1 | LIBUSB_ENDPOINT_IN),
-		packet.data(),
-		max_bytes_to_receive,
-		received_bytes,
-		timeout);
-}
-
-
-
-void odrive_endpoint_request(libusb_device_handle* handle, int endpoint_id, serial_buffer payload, int ack, int length) {
-	serial_buffer buffer;
-	int sent_bytes = 0;
-	int received_bytes = 0;
-	int max_bytes_to_receive = 64;
-	int timeout = 1000;
-	int test = 0;
-
-	buffer = create_odrive_packet(129, endpoint_id | 0x8000, (short)64, (int)0);
-
-	// Send the packet
-	send_to_odrive(handle, buffer, &sent_bytes);
-	// Immediatly wait for response from Odrive and check if ack (if we asked for one)
-	receive_from_odrive(handle, buffer, max_bytes_to_receive, &received_bytes, timeout);
-	// return the response payload
-	printf("%s\n", buffer.data());
-	test = 1;
-}
-
-
 int main() {
+
+	
+
 	libusb_context* ctx;
 	libusb_init(&ctx);
-	libusb_set_debug(ctx, 3);
-
+	//libusb_set_debug(ctx, 3);
 
 	libusb_device_handle* handle;
 	handle = libusb_open_device_with_vid_pid(ctx, VID, PID);
-
 	int r = libusb_claim_interface(handle, 1);
 
-	unsigned char chunk_buffer[64] = { 0 };
-	unsigned char buffer[100] = { 0 };
-	unsigned char message[] = { 129, 0,    // Seqno
-								0, 128,    // Endpoint
-								64, 0,   // Response size
-								0, 0, 0, 0,    // Payload
-								1, 0 };  // Protocol version / CRC16
+	serial_buffer receive_payload;
+	serial_buffer received_message;
+	int received_bytes = 128;
+	int total_received = 0;
+	json j;
 
-	serial_buffer payload;
+	while (received_bytes > 0) {
+		received_bytes = odrive_endpoint_request(handle, 0, receive_payload, total_received, 1, 64);
+		total_received += received_bytes;
+		for (uint8_t byte : receive_payload) {
+			received_message.push_back(byte);
+		}
+	}
 
-	int actual_send = 0;
-	int actual_receive = 0;
-	unsigned char test[] = { 0, 1, 2, 3 };
-
-	int chunk_length;
-
-	odrive_endpoint_request(handle, 0, payload, 1, 64);
-	/*
-	chunk_length = 64;
-	libusb_bulk_transfer(handle,
-		(1 | LIBUSB_ENDPOINT_OUT),
-		message,
-		ARRAY_SIZE(message),
-		&actual_send,
-		0);
-		
-	libusb_bulk_transfer(handle,
-		(1 | LIBUSB_ENDPOINT_IN),
-		chunk_buffer,
-		64,
-		&actual_receive,
-		1000);
-
-	*/
-
-
-	printf("Received %i bytes!\n", actual_receive);
-	printf("%s\n", chunk_buffer);
+	// json magic
+	j = json::parse(received_message);
+	//j[]
+	std::cout << std::setw(10) << j << "\n\n";
+	printf("\n Received %i bytes!\n", total_received);
 
 
 	libusb_release_interface(handle, 1);
--- src/protocol.h
+++ src/protocol.h
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <vector>
 #include <cstdint>
+#include "libusb-1.0\libusb.h"
 
 typedef std::vector<uint8_t> serial_buffer;
 
@@ -74,6 +75,8 @@ void deserialize(serial_buffer::iterator& it, T& t, U& u, Us&... us) {
 	deserialize(it, u, us...);
 }
 
+
+
 // Struct serialization
 void serialize(serial_buffer& serial_buffer, const odrive_packet& odrive_packet) {
 	serialize(serial_buffer, odrive_packet.sequence_number, odrive_packet.endpoint_id, odrive_packet.payload_length, odrive_packet.payload, odrive_packet.CRC16);
@@ -94,7 +97,7 @@ serial_buffer create_odrive_packet(short seq_no, short endpoint, short response_
 
 	serial_buffer data;
 
-	serialize(data, seq_no, endpoint, (short)response_size);
+	serialize(data, (short)seq_no, (short)endpoint, (short)response_size);
 
 	for (uint8_t b : payload) {
 		data.push_back(b);
@@ -103,4 +106,72 @@ serial_buffer create_odrive_packet(short seq_no, short endpoint, short response_
 	serialize(data, (short)(endpoint == 0 ? 0 : 1));
 
 	return data;
+}
+
+
+serial_buffer decode_odrive_packet(serial_buffer::iterator& it, short& seq_no, serial_buffer& received_packet) {
+	serial_buffer payload;
+	deserialize(it, seq_no);
+
+	// add this to a template?
+	while (it != received_packet.end()) {
+		payload.push_back(*it++);
+	}
+	return payload;
+}
+
+
+
+void send_to_odrive(libusb_device_handle* handle, std::vector<uint8_t>& packet, int* sent_bytes) {
+	libusb_bulk_transfer(handle,
+		(1 | LIBUSB_ENDPOINT_OUT),
+		packet.data(),
+		packet.size(),
+		sent_bytes,
+		0);
+}
+
+void receive_from_odrive(libusb_device_handle* handle, unsigned char* packet, int max_bytes_to_receive, int* received_bytes, int timeout = 0) {
+	libusb_bulk_transfer(handle,
+		(1 | LIBUSB_ENDPOINT_IN),
+		packet,
+		max_bytes_to_receive,
+		received_bytes,
+		timeout);
+}
+
+
+
+int odrive_endpoint_request(libusb_device_handle* handle, int endpoint_id, serial_buffer& received_payload, int count, int ack, int length) {
+	serial_buffer send_buffer;
+	serial_buffer receive_buffer;
+	unsigned char receive_bytes[100] = { 0 };
+	int sent_bytes = 0;
+	int received_bytes = 0;
+	int max_bytes_to_receive = 64;
+	int timeout = 1000;
+	int i = 0;
+	static short seq_no = 0;
+	short received_seq_no = 0;
+
+	seq_no = (seq_no + 1) & 0x7fff;
+	seq_no |= 0x80;
+	// Send the packet
+	send_to_odrive(handle, create_odrive_packet(seq_no, endpoint_id | 0x8000, (short)64, (int)count), &sent_bytes);
+
+
+	// Immediatly wait for response from Odrive and check if ack (if we asked for one)
+	receive_from_odrive(handle, receive_bytes, max_bytes_to_receive, &received_bytes, timeout);
+
+	//printf("%s\n", receive_bytes);
+
+	for (i = 0; i < received_bytes; i++) {
+		receive_buffer.push_back(receive_bytes[i]);
+	}
+
+	serial_buffer::iterator it = receive_buffer.begin();
+	received_payload = decode_odrive_packet(it, received_seq_no, receive_buffer);
+
+	// return the response payload
+	return received_payload.size();
 }
